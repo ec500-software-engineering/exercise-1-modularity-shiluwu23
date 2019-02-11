363d9692d3452d6e18340cf065aed34f6580bac9
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-11 00:13:42 -0500

    Update UserInterface_module.py

--- UserInterface_module.py
+++ UserInterface_module.py
@@ -1,7 +1,6 @@
 #copyright @Shilu Wu
 #It's the I/O documentation for the User_Interface_system
 #User Interface
-
 def alert_out(alert_number):
 	if alert_number == 0:
 		print("OK")
@@ -11,7 +10,6 @@ def alert_out(alert_number):
 		print("BP Alert!")
 	elif alert_number == 3:
 		print("PUL Alert!")
-
 def ai_output(predBloodOxygen,predBloodPressure,prePulse):
     print('predicted blood oxygen is: ' + str(predBloodOxygen))
     print('predicted blood pressure is: ' + str(predBloodPressure))
