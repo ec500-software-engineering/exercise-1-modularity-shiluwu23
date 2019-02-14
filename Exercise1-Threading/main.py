f1850ede9c5d15080e08cb4b044cad5078bd1317
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2019-02-14 18:06:39 -0500

    Update main.py

--- Exercise1-Threading/main.py
+++ Exercise1-Threading/main.py
@@ -1,42 +1,52 @@
+# Copyright by @Shilu Wu
+
 import Input_Module_lkn
+import storage
 import AiModule
-import Alert_module
+import Alert_Module_Xinsha
 import UserInterface_module
-#from queue import Queue
 import threading
-import time
-import random
 
+def generate(a,b,c):
+	bloodoxy = Input_Module_lkn.read_data(a)
+	bloodpressure = Input_Module_lkn.read_data(b)
+	pulse = Input_Module_lkn.read_data(c)
+	database = []
 
-def generate():
-    # INPUT
-    bloodoxy = Input_Module_lkn.read_data('./bloodoxy.txt')
-    bloodpressure = Input_Module_lkn.read_data('./bloodpressure.txt')
-    pulse = Input_Module_lkn.read_data('./pulse.txt')
-    #AI
-    AI = AiModule.AiModule()
-    AI.input_check(bloodoxy,bloodpressure,pulse)
-    a, b, c = AI.predict()
+	for i in range(len(bloodoxy)):
+		database.append(storage.storage(bloodoxy[i],bloodpressure[i],pulse[i]))
 
-    # Alert
-    alarm = Alert_module.Alert()
-    for i in range(len(bloodoxy)):
-        boi = bloodoxy[i], 0
-        bpi = bloodpressure[i], 1
-        puli = pulse[i], 2
-        boa = alarm.get_bo_data(boi)
-        bpa = alarm.get_bp_data(bpi)
-        pula = alarm.get_pul_data(puli)
-        #UserInterface
-    User = UserInterface_module.userInterface()
-    UserInterface_module.ai_output(a, b, c)
+	AI = AiModule.AiModule()
+	AI.input_check(bloodoxy,bloodpressure,pulse)
+	a,b,c=AI.predict()
 
+	UserInterface_module.ai_output(a,b,c)
 
+	alarm = Alert_Module_Xinsha.Alert()
 
+	for i in range(len(bloodoxy)):
+		alarm.get_bo_data(bloodoxy[i])
+		alarm.get_bp_data(bloodpressure[i])
+		alarm.get_pul_data(pulse[i])
 
-while True:
-	for i in range(5):
-		t = threading.Thread(target = generate)
-		t.start()
-		time.sleep(2)
+	UserInterface_module.alert_out(alarm.Alert_Output())
 
+if __name__ == '__main__':
+
+	a = './bloodoxy1.txt'
+	b = './bloodpressure1.txt'
+	c = './pulse1.txt'
+	t1 = threading.Thread(target=generate(a,b,c))
+	t1.start()
+
+	a = './bloodoxy2.txt'
+	b = './bloodpressure2.txt'
+	c = './pulse2.txt'
+	t2 = threading.Thread(target=generate(a,b,c))
+	t2.start()
+
+	a = './bloodoxy3.txt'
+	b = './bloodpressure3.txt'
+	c = './pulse3.txt'
+	t3 = threading.Thread(target=generate(a,b,c))
+	t3.start()
